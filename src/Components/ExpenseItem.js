cf86d314605c31ad18f5b6eb21228bb896caf236
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-02-18 11:21:55 +0530

    folowed trainers code

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -4,19 +4,15 @@ import ExpenseDetails from "./ExpenseDetails";
 import Card from "./Card";
 function ExpenseItem(props){
     return (
-      <div className='expense-item'>
-			<ExpenseDate date ={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails>
-		</div>
-      // <Card className="expense-item">
-      //   <ExpenseDate date={props.date} />
-      //  <div className="expense-item__description">
-      //  <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails>
-      //      {/* <h2 className="expense-item h2">{props.title}</h2>
-      //     <div className="expense-item__price">Rs.{props.amount}/-</div>
-      //     <div className="expense-item__location">{props.location}</div>  */}
-      //   </div> 
-      // </Card>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+       <div className="expense-item__description">
+       {/* <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails> */}
+           <h2 className="expense-item h2">{props.title}</h2>
+          <div className="expense-item__price">Rs.{props.amount}/-</div>
+          <div className="expense-item__location">{props.location}</div> 
+        </div> 
+      </Card>
     );
 }
 
