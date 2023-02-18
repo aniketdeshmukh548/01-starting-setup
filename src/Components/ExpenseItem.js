1ada9e88d115b476f3ea21c1cb79385c42c9acc8
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-02-18 11:18:58 +0530

    task 10 adding expense details component

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,15 +1,22 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import Card from "./Card";
 function ExpenseItem(props){
     return (
-      <Card className="expense-item">
-        <ExpenseDate date={props.date}></ExpenseDate>
-        <div className="expense-item__description">
-          <h2 className="expense-item h2">{props.title}</h2>
-          <div className="expense-item__price">Rs.{props.amount}/-</div>
-        </div>
-      </Card>
+      <div className='expense-item'>
+			<ExpenseDate date ={props.date} />
+      <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails>
+		</div>
+      // <Card className="expense-item">
+      //   <ExpenseDate date={props.date} />
+      //  <div className="expense-item__description">
+      //  <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails>
+      //      {/* <h2 className="expense-item h2">{props.title}</h2>
+      //     <div className="expense-item__price">Rs.{props.amount}/-</div>
+      //     <div className="expense-item__location">{props.location}</div>  */}
+      //   </div> 
+      // </Card>
     );
 }
 
