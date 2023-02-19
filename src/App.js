82a5da242ab94dcf46d87f81fcdc4626a55340f4
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-02-19 13:24:33 +0530

    Adding buttons

--- src/App.js
+++ src/App.js
@@ -33,22 +33,22 @@ const App = () => {
       location: "Mumbai",
     },
   ];
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started!"),
-    React.createElement(Expenses,{items:expenses})
-  );
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses}></Expenses>
-  //     {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} location={expenses[0].location}></ExpenseItem>
-  //     <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location}></ExpenseItem>
-  //     <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location}></ExpenseItem>
-  //     <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} location={expenses[3].location}></ExpenseItem>  */}
-  //   </div>
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses,{items:expenses})
   // );
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}></Expenses>
+      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} location={expenses[0].location}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} location={expenses[3].location}></ExpenseItem>  */}
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,15 +4,25 @@ import Card from '../UI/Card';
 import "./ExpenseItem.css";
 //import ExpenseDetails from "./ExpenseDetails";
 const ExpenseItem=(props)=> {
+  let title=props.title;
+  let amount=props.amount
+  const clickHandler=()=>{
+    title='updated';
+  }
+  const deleteEvent=()=>{
+    amount='deleted';
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         {/* <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails> */}
-        <h2 className="expense-item h2">{props.title}</h2>
+        <h2 className="expense-item h2">{title}</h2>
         <div className="expense-item__location">{props.location}</div>
-        <div className="expense-item__price">Rs.{props.amount}/-</div>
+        <div className="expense-item__price">Rs.{amount}/-</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteEvent}>Delete</button>
     </Card>
   );
 }
